248247af74409bb2a7b18c987a9432639fb11ca2
Clara <csplicidobaida@gmail.com>
2025-09-23 11:38:27 -0400

    Rota Carrinho

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,3 +1,4 @@
+import carrinhoController from "./carrinho/carrinho.controller.js";
 import produtoController from "./produtos/produto.controller.js";
 import usuarioController from "./usuarios/usuario.controller.js";   
 
@@ -11,4 +12,5 @@ rotas.get('/usuarios', usuarioController.listar);
 rotas.post('/produtos', produtoController.adicionar);
 rotas.get('/produtos', produtoController.listar);
 
+rotas.post('/carrinhos', carrinhoController.adicionarItem);
 export default rotas;
